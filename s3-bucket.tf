72e4bd5d57c21eae80de5b9c5616b94ca9bda06f
govin <govin.r8@gmail.com>
2021-08-11 18:10:13 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,7 @@
 module "s3_bucket" {
   source = "app.terraform.io/govin-training/s3-bucket/aws"
   version = "2.2.0"
-  bucket        = local.bucket_name
-  acl           = "private"
-  force_destroy = true
+  bucket        = local.govin
 }
 
 
